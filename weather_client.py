ea867b7d6e53bc6f9baf60905005b69b173c0333
victorialawton <73839142+victorialawton@users.noreply.github.com>
2024-01-30 16:29:17 -0800

    Update weather_client.py

--- weather_client.py
+++ weather_client.py
@@ -7,7 +7,7 @@ from typing import Dict
 URL = "https://api.openweathermap.org/data/2.5/weather"
 
 # TODO: get an API key from openweathermap.org and fill it in here!
-API_KEY = ""
+API_KEY = "47d1e398db25f256cf6fb38dc60ef3c8"
 
 def get_weather(city) -> Dict:
     res = requests.get(URL, params={"q": city, "appid": API_KEY})
